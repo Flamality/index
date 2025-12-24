25bdb532eb8f69f66853579841fbe77eb504daaa
Remi <urflamality@gmail.com>
2025-12-24 01:18:06 -0500

    Added Banner

---
+++ src/Landing/Components/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Banner.module.css";
+import { databases } from "../../../services/appwrite";
+export default function Banner() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        databases.getDocument("main", "flags", "root_banner").then((res) => {
+          setData(res);
+        });
+      } catch (error) {}
+    };
+    getData();
+  }, []);
+  return (
+    <div className={styles.banner}>
+      <p className={styles.header}>{data?.title}</p>
+      <p className={styles.content}>{data?.desc}</p>
+    </div>
+  );
+}
--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -12,46 +12,48 @@ import Footer from "./Components/Footer/Footer";
 import { Layers } from "../contexts/layers";
 import Popup from "../components/core/elements/Modal/Modals/Popup/Popup";
 import NavBar from "../components/ui/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
 function App() {
-	const {showModal} = useContext(Layers)
+  const { showModal } = useContext(Layers);
 
-	useEffect(() => {
-		document.title = "Flamality";
-		const width = window.innerWidth;
-		const height = window.innerHeight;
-		// showModal('center', <Popup title="Screen Size"  desc={`Width: ${width} | Height: ${height}`} />);
-	}, []);
-	return (
-		<div className={styles.landing}>
-			{/* <NavBar /> */}
-			<div className={styles.top}>
-				<div className={styles.background} />
-				<Header />
-				<DiscordCard />
-				<AccountCard />
-				<ZoneZero />
-				<div className={styles.link_cards}>
-					<LinkCard
-						text={"My Portfolio"}
-						link={"portfolio.flamality.com"}
-						icon={GoGlobe}
-					/>
-					<LinkCard
-						text={"My GitHub"}
-						link={"github.com/Flamality"}
-						icon={FaGithub}
-					/>
-					<LinkCard
-						text={"Create a Flamality account"}
-						link={"flamality.com/account"}
-						icon={FaUser}
-					/>
-				</div>
-			</div>
-			<div className={styles.spacing} />
-			<Footer />
-		</div>
-	);
+  useEffect(() => {
+    document.title = "Flamality";
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    // showModal('center', <Popup title="Screen Size"  desc={`Width: ${width} | Height: ${height}`} />);
+  }, []);
+  return (
+    <div className={styles.landing}>
+      {/* <NavBar /> */}
+      <Banner />
+      <div className={styles.top}>
+        <div className={styles.background} />
+        <Header />
+        <DiscordCard />
+        <AccountCard />
+        <ZoneZero />
+        <div className={styles.link_cards}>
+          <LinkCard
+            text={"My Portfolio"}
+            link={"portfolio.flamality.com"}
+            icon={GoGlobe}
+          />
+          <LinkCard
+            text={"My GitHub"}
+            link={"github.com/Flamality"}
+            icon={FaGithub}
+          />
+          <LinkCard
+            text={"Create a Flamality account"}
+            link={"flamality.com/account"}
+            icon={FaUser}
+          />
+        </div>
+      </div>
+      <div className={styles.spacing} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
