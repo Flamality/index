a0757f594b3f3c415420f9b5487d156ac130aeb6
Remi <urflamality@gmail.com>
2026-08-19 16:35:50 -0400

    Fix

--- src/components/core/elements/overlays/ToolTip/ToolTip.jsx
+++ src/components/core/elements/overlays/ToolTip/ToolTip.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./ToolTip.module.css";
+import styles from "./tooltip.module.css";
 export default function ToolTip({ position = "top", content = "", children }) {
   const [visible, setVisible] = useState(false);
 
